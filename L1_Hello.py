b5c8a5fa320db8b4b324d4d20a2477e7a6b534fb
Sudipta987 <rajakar21162@gmail.com>
2026-06-02 20:34:37 +0530

    L1-First Python program — print, comments

---
+++ L1_Hello.py
@@ -0,0 +1,7 @@
+# This is an single line comment in Python and this is the first program in Python
+""" this is an multiline comment 
+technique in Python not officail"""
+print("Hello\nWorld")
+
+print(23+54)
+
