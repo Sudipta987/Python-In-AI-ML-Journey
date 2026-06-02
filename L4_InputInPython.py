b3f63d820d9e9edb392ec79d460900141df46721
Sudipta987 <rajakar21162@gmail.com>
2026-06-02 20:43:27 +0530

    L4 - User input, type casting

---
+++ L4_InputInPython.py
@@ -0,0 +1,19 @@
+#Here We will see how can we take an input from the user in Python
+
+# String Input
+
+name=input("Enter Your name:")
+
+#int Input
+
+age=int(input("Enter your Age: "))
+
+#Float input 
+price=float(input("Enter the price: "))
+
+print(name)
+print(age)
+print(price)
+
+print(type(name))  #try taking name input other than string value and you will find that it consider all data types as string 
+
