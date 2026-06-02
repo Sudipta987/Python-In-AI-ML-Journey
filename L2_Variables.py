b505df71b6fcdfdd89fc48dfa23f4d23aefe6f2a
Sudipta987 <rajakar21162@gmail.com>
2026-06-02 20:38:50 +0530

    L2 - Variables, data types, type inference

---
+++ L2_Variables.py
@@ -0,0 +1,33 @@
+#In python special symboles are alos not allowed 
+var1="value1"
+#1var=34 # is not an valid variable in python
+name="Sudipta kar"
+char ='A'
+age=22
+price=24.99
+a=None
+isTrue=True
+
+print(a)
+print(type(a))
+print(isTrue)
+print(type(isTrue))
+print(type(name))
+print(type(char))
+print(type(age))
+print(type(price))
+
+# Data Types on Python
+# Integers(e.g., +ve,-ve,0)
+# String (e.g., "Sudipta kar"| 'Hunter' | '''dsa''')
+# Float (e.g, 3.56)
+# Boolean (e.g, T, F)
+# None (e.g, a=None)
+
+#Type Conversion/casting of Python
+a=float("2")
+b=4.75
+print(type(a))
+print(a+b)
+c=str(3.14)
+print(type(c))
